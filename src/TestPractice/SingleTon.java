1e0f84b280beecd6f9969e1fb347908553c8b5c8
vipul <vipul@ASH-NUC-05.ashsoftware.local>
2018-06-14 11:18:20 +0530

    temp demo

---
+++ src/TestPractice/SingleTon.java
@@ -0,0 +1,22 @@
+package TestPractice;
+
+public class SingleTon {
+
+	
+	public static SingleTon sTon = new SingleTon();
+	
+	private SingleTon()
+	{		
+	};
+	
+	
+	
+	
+	public static void main(String[] args)
+	
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
